c6245afade74e6768eb7cfbf26bd2554d6bd9ab2
upexcel <rahul.excel2011@gmail.com>
2018-07-25 18:26:55 +0530

    used redux-actions library

--- src/Actions.js
+++ src/Actions.js
@@ -1,26 +1,7 @@
-export const ADD_TODO = "ADD_TODO";
-export const EDIT_TODO = "EDIT_TODO";
-export const TOGGLE_TODO = "TOGGLE_TODO";
+import { createActions } from "redux-actions";
 
-export const addTodo = (text, id) => {
-  return {
-    type: ADD_TODO,
-    text,
-    id
-  };
-};
-
-export const editTodo = (text, id) => {
-  return {
-    type: EDIT_TODO,
-    text,
-    id
-  };
-};
-
-export const toggleTodo = id => {
-  return {
-    type: TOGGLE_TODO,
-    id
-  };
-};
+export const { addTodo, editTodo, toggleTodo } = createActions({
+  ADD_TODO: (text, id) => ({ text, id }),
+  EDIT_TODO: (text, id) => ({ text, id }),
+  TOGGLE_TODO: id => ({ id })
+});
--- src/Reducers.js
+++ src/Reducers.js
@@ -1,33 +1,41 @@
-import { ADD_TODO, EDIT_TODO, TOGGLE_TODO } from "./Actions";
 import clone from "lodash/clone";
 import map from "lodash/map";
 import pullAt from "lodash/pullAt";
-const initialState = { todo: [] };
+import { handleActions } from "redux-actions";
+import { addTodo, editTodo, toggleTodo } from "./Actions";
 
-export const TodoApp = (state = initialState, action) => {
-  let t = clone(state.todo);
-  switch (action.type) {
-    case ADD_TODO:
-      return {
-        todo: [...t, { id: action.id, text: action.text, completed: false }]
-      };
-    case TOGGLE_TODO:
-      return {
-        todo: map(t, (todo, index) => {
-          if (index === action.id) {
-            return Object.assign({}, todo, { completed: !todo.completed });
-          }
-          return todo;
-        })
-      };
-    case EDIT_TODO:
-      if (action.text === "") {
-        pullAt(t, action.id);
-      } else {
-        t[action.id].text = action.text;
-      }
-      return { todo: t };
-    default:
-      return state;
-  }
+const defaultState = { todo: [] };
+
+const handleAddTodo = (state, { payload: { text, id } }) => {
+  return {
+    todo: [...state.todo, { id: id, text: text, completed: false }]
+  };
 };
+const handleToggleTodo = (state, { payload: { id } }) => {
+  return {
+    todo: map(state.todo, (todo, index) => {
+      if (index === id) {
+        return Object.assign({}, todo, { completed: !todo.completed });
+      }
+      return todo;
+    })
+  };
+};
+const handleEditTodo = (state, { payload: { text, id } }) => {
+  let t = clone(state.todo);
+  if (text === "") {
+    pullAt(t, id);
+  } else {
+    t[id].text = text;
+  }
+  return { todo: t };
+};
+
+export const TodoApp = handleActions(
+  {
+    [addTodo]: handleAddTodo,
+    [toggleTodo]: handleToggleTodo,
+    [editTodo]: handleEditTodo
+  },
+  defaultState
+);
